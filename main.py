860edfcd284495d77485dd8760592a1713d553e6
хурма 502 <polya06022006@gmail.com>
2025-04-12 22:43:04 +0300

    Добавляет функцию регистрации, записывает данные о пользователях в таблицу csv

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import telebot
+import telebot # Работа с ботом
+import csv # Работа с базой пользователей
 from config import token
 
 # Создание бота
@@ -24,6 +25,7 @@ def register(message):
     chat_id = message.chat.id
     bot.send_message(chat_id, "Введите пароль для регистрации:")
     # Сохраняем пароль пользователя
+    bot.register_next_step_handler(message, add_user)
 
 # Обработчик команды /login
 @bot.message_handler(commands=['login'])
@@ -42,6 +44,37 @@ def predict(message):
 def logout(message):
     chat_id = message.chat.id
     bot.send_message(chat_id, "Сначала войдите с помощью /login.")
+
+def find_user(user_id):
+    user_count = 0
+    with open('base/users.csv', 'r', encoding='utf-8') as base:
+        reader = csv.DictReader(base)  # Читаем как словарь (удобно для работы с колонками)
+        for row in reader:
+            user_count = user_count + 1
+            if row['User_ID'] == str(user_id):
+                return row['ID'], row
+    return user_count + 1, None
+
+def add_user(message):
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    user_name = message.from_user.username
+    password = message.text
+    try:
+        ID, row = find_user(user_id)
+        if row == None:
+            with open('base/users.csv', 'a', newline='', encoding='utf-8') as base:
+                writer = csv.writer(base)
+                writer.writerow([ID, user_id, chat_id, user_name, password])
+            bot.send_message(chat_id, "Поздравляю, вы успешно зарегистрированы!")
+        else:
+            bot.send_message(chat_id, "Вы уже ранее были зарегистрированы")
+    except PermissionError:
+        print("Нет прав на запись в файл!")
+    except csv.Error as e:
+        print(f"Ошибка парсинга CSV: {e}")
+    except Exception as e:
+        print(e)
 
 #Non-stop working mode
 bot.infinity_polling()
